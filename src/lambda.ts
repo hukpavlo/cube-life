0b871f2e0683b6b12855b9cf303aa3845181a8a5
hukpavlo <pavlo2001lviv@gmail.com>
2020-05-17 14:12:54 +0300

    Added swagger for lambda

--- src/lambda.ts
+++ src/lambda.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as awsServerlessExpress from 'aws-serverless-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -16,7 +17,16 @@ const bootstrapServer = async (): Promise<Server> => {
 
   const app = await NestFactory.create(AppModule, adapter);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.init();
 
   return awsServerlessExpress.createServer(expressApp);
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('Cats example')
